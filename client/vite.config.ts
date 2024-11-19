3b66b1629ba1bd5d6d62f02325f5f622578e8b0b
Manu Vanneste <125195959+manulykebe@users.noreply.github.com>
2024-11-19 15:27:18 +0100

    setup

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  root: './', // Ensure this points to the correct directory
+  build: {
+    outDir: 'dist',
+  },
   server: {
     port: 3000
   }
