e82c7a2a49886a3e1edcae3aa030e9e4aebfab0c
Manu Vanneste <125195959+manulykebe@users.noreply.github.com>
2025-01-22 23:20:39 +0100

    Update config.ts

--- src/lib/api/config.ts
+++ src/lib/api/config.ts
@@ -1,7 +1,7 @@
-// import { IS_PRODUCTION } from "../../../server/config.js";
+const IS_PRODUCTION = import.meta.env.PROD;
 
-// export const API_URL = IS_PRODUCTION
-//   ? "https://team-calendar.onrender.com/api"
-//   : "http://localhost:3000/api";
+export const API_URL = IS_PRODUCTION
+  ? "https://team-calendar.onrender.com/api"
+  : "http://localhost:3000/api";
 
-export const API_URL = "https://team-calendar.onrender.com/api";
+// export const API_URL = "https://team-calendar.onrender.com/api";
