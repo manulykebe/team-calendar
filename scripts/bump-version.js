5a938c99214db5cee7dd90907eee48d56c527cbf
Manu Vanneste <manu@lyke.be>
2025-06-13 16:09:38 +0200

    version bump

--- scripts/bump-version.js
+++ scripts/bump-version.js
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const path = require('path');
+import { fileURLToPath } from 'url';
+import path from 'path';
+import fs from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 function bumpVersion() {
   const packageJsonPath = path.join(__dirname, '..', 'package.json');
