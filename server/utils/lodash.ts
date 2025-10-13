6145618a9756e13362a914dfaf9dfdaee2123545
Manu Vanneste <manu@lyke.be>
2025-10-13 17:58:27 +0200

    Refactor lodash exports for selective named exports
    Replaces wildcard export from lodash with explicit named exports for commonly used functions. This change improves tree-shaking and clarity by only exposing selected lodash utilities.

--- server/utils/lodash.ts
+++ server/utils/lodash.ts
@@ -1,2 +1,13 @@
-export * from 'lodash';
-export { default as default } from 'lodash';
+import * as _ from 'lodash';
+
+// Re-export all named functions
+export const {
+  get, set, merge, find, findIndex, sortBy, groupBy, debounce, throttle,
+  cloneDeep, isEqual, isEmpty, isArray, isObject, isString, isNumber,
+  isBoolean, isUndefined, isNull, pick, omit, uniq, uniqBy, flatten,
+  chunk, difference, differenceBy, intersection, union, range
+  // Add other lodash functions you need
+} = _;
+
+// Export default
+export default _;
